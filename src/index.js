df02c0ccf20a46cde1d1bc39e954c5c4174ae3b3
Sneha kumari <snehakumari8801@gmail.com>
2024-04-16 16:42:04 +0530

    utlils code

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ dotenv.config({
     path:'./env'
 })
 
-connect_DB() 
+connect_DB()
+
+.then(()=>{
+  app.listen(process.env.PORT || 8000 , () =>{
+    console.log(`Server is running at port ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log('MONGO db connection failed!!!' , err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+import { Promise } from "mongoose"
+
+///----------------------------------method 1---------------------------------------///
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) =>{
+      Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export { asyncHandler }
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (func) => () =>{}           function pass into another function
+// const asyncHandler = (func) => async() =>{}
+
+
+
+///-----------------------------------method 2--------------------------------------------///
+// const asyncHandler = (func) => async (res , req ,next) =>{
+//     try {
+//         await func(req , res ,next)
+//     } catch (error) {
+//         res.status(res.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
